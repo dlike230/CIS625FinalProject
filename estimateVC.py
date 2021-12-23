a55d32eb3e30bc324255c232ec222a53c1a1924d
dlike <dlike@seas.upenn.edu>
2021-12-22 20:52:23 -0500

    add sauer's lemma estimation (currently broken)

--- estimateVC.py
+++ estimateVC.py
@@ -1,40 +1,69 @@
+import math
 import random
 
 
-def draw_random_labeled_sample(draw_random_example, sample_size):
+def random_labeled_sample(draw_random_example, sample_size):
     return [(draw_random_example(), random.randint(0, 1)) for _ in range(sample_size)]
 
 
 def estimate_can_shatter(draw_random_example, can_induce, sample_size, num_trials):
     for _ in range(num_trials):
-        sample = draw_random_labeled_sample(draw_random_example, sample_size)
-        if not can_induce(sample):
+        labeled_sample = random_labeled_sample(draw_random_example, sample_size)
+        if not can_induce(labeled_sample):
             return False
     return True
 
 
-def estimate_vc_shattering(draw_random_example, can_induce, num_trials=2000):
+def log_phi(d, m):
+    return d * math.log2(math.e * m / d)
+
+
+def verify_saeur(draw_random_example, can_induce, vcd, num_trials):
+    total_labelings_induced = 0
+    sample_size = vcd * 2
+    for _ in range(num_trials):
+        labeled_sample = random_labeled_sample(draw_random_example, sample_size)
+        if can_induce(labeled_sample):
+            total_labelings_induced += 1
+
+    # print(log_phi(vcd, sample_size) - sample_size, vcd, sample_size)
+    # expected_labelings_induced = num_trials * (2 ** (log_phi(vcd, sample_size) - sample_size))
+
+    if total_labelings_induced == 0:
+        return True
+
+    log_est_labels_induced = math.log2(total_labelings_induced / num_trials) + sample_size
+
+    return log_est_labels_induced < log_phi(vcd, sample_size)
+
+
+def estimate_vc_bin_search(draw_random_example, can_induce, consistent_vc):
     # estimate a lower and upper bound on the VC dimension before performing a binary search for the VC dimension
     lower_bound = 1
     upper_bound = 1
-    while estimate_can_shatter(draw_random_example, can_induce, upper_bound, num_trials):
+    while consistent_vc(draw_random_example, can_induce, upper_bound):
         lower_bound = upper_bound
         upper_bound *= 2
 
     # perform a binary search for the estimated VC dimension
     while abs(lower_bound - upper_bound) > 1:
         middle = (lower_bound + upper_bound) // 2
-        if estimate_can_shatter(draw_random_example, can_induce, middle, num_trials):
+        if consistent_vc(draw_random_example, can_induce, middle):
             lower_bound = middle
         else:
             upper_bound = middle
 
-    if not estimate_can_shatter(draw_random_example, can_induce, upper_bound, num_trials):
+    if not consistent_vc(draw_random_example, can_induce, upper_bound):
         return lower_bound
 
     return upper_bound
 
 
-def estimate_vc_shattering_improved(draw_random_example, can_induce, vc_prior=2):
-    vc1 = estimate_vc_shattering(draw_random_example, can_induce, vc_prior * 1000)
-    return estimate_vc_shattering(draw_random_example, can_induce, vc1 * 1000)
+def estimate_vc_shattering(draw_random_example, can_induce, num_trials=2000):
+    consistent_vc = lambda draw, check, sample_size: estimate_can_shatter(draw, check, sample_size, num_trials)
+    return estimate_vc_bin_search(draw_random_example, can_induce, consistent_vc)
+
+
+def estimate_vc_sauer(draw_random_example, can_induce, num_trials=2000):
+    consistent_vc = lambda draw, check, vc: verify_saeur(draw, check, vc, num_trials) and estimate_can_shatter(draw, check, vc, num_trials)
+    return estimate_vc_bin_search(draw_random_example, can_induce, consistent_vc)
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import random
 
-from estimateVC import estimate_vc_shattering_improved
+from estimateVC import estimate_vc_sauer, estimate_vc_shattering
 from induceLabels import union_of_intervals
 
 if __name__ == "__main__":
     for i in range(100):
         make_example = lambda: random.random()
         induce_labels = lambda s: union_of_intervals(s, i)
-        print(i, ":", estimate_vc_shattering_improved(make_example, induce_labels))
+        print(i, ":", estimate_vc_sauer(make_example, induce_labels))
